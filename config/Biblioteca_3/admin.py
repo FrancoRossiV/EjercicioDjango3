26397c1f6bab4154767ce1235f87cbf97e0c9084
FrancoRossiV <francoagustin433@gmail.com>
2020-08-19 16:44:44 -0300

    Added models and their respective configurtaions; project start up

--- config/Biblioteca_3/admin.py
+++ config/Biblioteca_3/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from Biblioteca_3.models import *
+
 
 # Register your models here.
+admin.site.register(Prestamo)
+admin.site.register(Material)
+admin.site.register(Libro)
+admin.site.register(Revista)
+admin.site.register(Persona)
+admin.site.register(Alumno)
+admin.site.register(Profesor)
--- config/Biblioteca_3/models.py
+++ config/Biblioteca_3/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+class Material(models.Model):
+    codigo = models.AutoField(primary_key=True)
+    tipoMaterial = models.CharField(max_length=50)
+    autor = models.CharField(max_length=50)
+    titulo = models.CharField(max_length=50)
+    anio = models.DateField()
+    status= models.CharField(max_length=50)
+
+
+class Libro(Material):
+    editorial = models.CharField(max_length=50)    
+
+class Revista(Material):
+    paginas = models.IntegerField()
+
+class Persona(models.Model):
+    tipoPersona = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    correo = models.CharField(max_length=50)
+    telefono = models.IntegerField()
+    numLibros = models.IntegerField()
+    adeudo = models.IntegerField()
+
+class Alumno(Persona):
+    matricula = models.IntegerField()
+
+class Profesor(Persona):
+    numEmpleado = models.IntegerField()   
+
+
+class Prestamo(models.Model):
+    codigo = models.AutoField(primary_key=True)
+    fechaSalida = models.DateField()
+    fechaRegreso = models.DateField()
+    prestamoLibro = models.ManyToManyField(Libro, blank = True)
+    prestamoRevista = models.ManyToManyField(Revista, blank = True)  
+    prestamoAlumno = models.ForeignKey(Alumno, on_delete=models.CASCADE, default = None, blank = True)  
+    prestamoProfesor = models.ForeignKey(Profesor, on_delete=models.CASCADE, default = None, blank = True) 
+
