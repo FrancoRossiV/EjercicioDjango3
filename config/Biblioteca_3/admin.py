3afd0ef47b20f70164c4f52aeba59ad11b197f13
FrancoRossiV <francoagustin433@gmail.com>
2020-08-19 20:14:02 -0300

    Arreglando varios errores y empezando a finalizar el proyecto

--- config/Biblioteca_3/admin.py
+++ config/Biblioteca_3/admin.py
@@ -2,11 +2,53 @@ from django.contrib import admin
 from Biblioteca_3.models import *
 
 
+
+class PrestamoInLine(admin.TabularInline):
+    model = Prestamo
+
+
+class AlumnoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'correo', 'telefono','numLibros', 'adeudo')
+    inlines = [PrestamoInLine,]
+
+    fieldsets = (
+        ('Datos Personales', {
+            'fields': ('nombre', 'apellido')
+        }),
+        ('Contacto', {
+            'fields': ('correo', 'telefono', 'numLibros', 'adeudo')
+        })
+    )
+
+class ProfesorAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'correo', 'telefono','numEmpleado', 'numLibros', 'adeudo')
+
+    fieldsets = (
+        ('Datos Personales', {
+            'fields': ('nombre', 'apellido')
+        }),
+        ('Contacto', {
+            'fields': ('correo', 'telefono', 'numEmpleado', 'numLibros', 'adeudo')
+        })
+    )
+
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ('autor', 'titulo', 'anio', 'tipoMaterial','status', 'editorial')
+    list_filter = ('titulo',)
+
+class RevistaAdmin(admin.ModelAdmin):
+    list_display = ('autor', 'titulo', 'anio', 'tipoMaterial','status', 'paginas')
+    list_filter = ('titulo',)
+
+class PrestamoAdmin(admin.ModelAdmin):
+    list_display = ('fechaSalida', 'fechaRegreso','prestamoAlumno', 'prestamoProfesor')
+    
+
 # Register your models here.
-admin.site.register(Prestamo)
-admin.site.register(Material)
-admin.site.register(Libro)
-admin.site.register(Revista)
+admin.site.register(Prestamo, PrestamoAdmin)
+admin.site.register(Material, )
+admin.site.register(Libro, LibroAdmin)
+admin.site.register(Revista, RevistaAdmin)
 admin.site.register(Persona)
-admin.site.register(Alumno)
-admin.site.register(Profesor)
+admin.site.register(Alumno, AlumnoAdmin)
+admin.site.register(Profesor, ProfesorAdmin)
--- config/Biblioteca_3/models.py
+++ config/Biblioteca_3/models.py
@@ -10,12 +10,23 @@ class Material(models.Model):
     anio = models.DateField()
     status= models.CharField(max_length=50)
 
+    def __str__(self):
+        return "Material: " + str(self.Codigo) + " " + str(self.tipoMaterial) + " " + str(self.autor) + " " + str(self.titulo) + " " + str(self.anio) + " " + str(self.status)
+
 
 class Libro(Material):
-    editorial = models.CharField(max_length=50)    
+    editorial = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "Libro: " + str(self.codigo) + " " + str(self.tipoMaterial) + " " + str(self.autor) + " " + str(self.titulo) + " " + str(self.anio) + " " + str(self.status) + " " + str(self.editorial)
+    
 
 class Revista(Material):
     paginas = models.IntegerField()
+
+    def __str__(self):
+        return "Revista: " + str(self.codigo) + " " + str(self.tipoMaterial) + " " + str(self.autor) + " " + str(self.titulo) + " " + str(self.anio) + " " + str(self.status) + " " + str(self.paginas)
+
 
 class Persona(models.Model):
     tipoPersona = models.CharField(max_length=50)
@@ -24,13 +35,25 @@ class Persona(models.Model):
     correo = models.CharField(max_length=50)
     telefono = models.IntegerField()
     numLibros = models.IntegerField()
-    adeudo = models.IntegerField()
+    adeudo = models.IntegerField(null = True)
+
+    def __str__(self):
+        return "Persona: " + str(self.tipoPersona) + " " + str(self.nombre) + " " + str(self.apellido) + " " + str(self.correo) + " " + str(self.telefono) + " " + str(self.numLibros) + " " + str(self.adeudo)
+
 
 class Alumno(Persona):
     matricula = models.IntegerField()
 
+    def __str__(self):
+        return "Alumno: " + str(self.tipoPersona) + " " + str(self.nombre) + " " + str(self.apellido) + " " + str(self.correo) + " " + str(self.telefono) + " " + str(self.numLibros) + " " + str(self.adeudo) + " " + str(self.matricula)
+
+
+
 class Profesor(Persona):
     numEmpleado = models.IntegerField()   
+
+    def __str__(self):
+        return "Persona: " + str(self.tipoPersona) + " " + str(self.nombre) + " " + str(self.apellido) + " " + str(self.correo) + " " + str(self.telefono) + " " + str(self.numLibros) + " " + str(self.adeudo) + " " + str(self.numEmpleado)
 
 
 class Prestamo(models.Model):
@@ -41,4 +64,7 @@ class Prestamo(models.Model):
     prestamoRevista = models.ManyToManyField(Revista, blank = True, null = True)  
     prestamoAlumno = models.ForeignKey(Alumno, on_delete=models.CASCADE, default = None, blank = True, null = True)  
     prestamoProfesor = models.ForeignKey(Profesor, on_delete=models.CASCADE, default = None, blank = True, null = True) 
+
+    def __str__(self):
+        return "Prestamo: " + str(self.codigo) + " " + str(self.fechaSalida) + " " + str(self.fechaRegreso) + " " + str(self.prestamoLibro) + " " + str(self.prestamoRevista) + " " + str(self.prestamoAlumno) + " " + str(self.prestamoProfesor) 
 
